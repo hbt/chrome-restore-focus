66c1da6591aa0e7bc1fe1c94bd79c0b23d321fb5
hbt <hassenbentanfous@gmail.com>
2018-12-03 22:37:30 -0500

    wip

--- extension-reloader.js
+++ extension-reloader.js
@@ -1,29 +1,31 @@
-'use strict';
+"use strict";
 
 class ExtensionReloader {
+    start() {
+        var lastModified = new Date();
 
-  start() {
-    var lastModified = (new Date());
-    
-    window.setInterval(v => {
-      chrome.runtime.getPackageDirectoryEntry(dir => {
-        dir.getFile('.extension-reloader', {}, fe => {
-          fe.file(file => {
-            if (file.lastModified > lastModified) {
-              chrome.runtime.reload();
-            }
-          }, null);
-        }, err => {
-          console.error(err);
-        });
-      });
-    }, 500);
-  }
+        window.setInterval(v => {
+            chrome.runtime.getPackageDirectoryEntry(dir => {
+                dir.getFile(
+                    ".extension-reloader",
+                    {},
+                    fe => {
+                        fe.file(file => {
+                            if (file.lastModified > lastModified) {
+                                chrome.runtime.reload();
+                            }
+                        }, null);
+                    },
+                    err => {
+                        console.error(err);
+                    }
+                );
+            });
+        }, 500);
+    }
 }
 
 var er = new ExtensionReloader();
-
-
 
 //er.start();
 //chrome.tabs.query({}, tabs => {
